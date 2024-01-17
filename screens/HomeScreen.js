9f1ed81948f38fc968261125bf07e43f25be0164
muneneee <lukekevin39@gmail.com>
2024-01-17 21:45:18 +0300

    Added logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
-import React from 'react'
+import React from 'react';
+import uberlogo from '../assets/uberlogo.png';
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>I am HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+        <View style={tw`p-5`}>
+            <Image 
+                style={{
+                    width: 100,
+                    height: 100,
+                    resizeMode: "contain",
+                }}
+                 source={uberlogo}
+
+            />
+           <Text>yfyfyfyy</Text> 
+
+           <NavOptions />
+        </View>
+
     </SafeAreaView>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    }
+})
