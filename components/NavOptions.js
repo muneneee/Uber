75a5e45bdcd1a4fafc4cb329643150a989d60498
muneneee <lukekevin39@gmail.com>
2024-02-09 13:01:13 +0300

    Iconscreen

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import { Icon  } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 
 const data = [
@@ -20,13 +21,18 @@ const data = [
 ];
 
 const NavOptions = () => {
+  const navigation = useNavigation();
+
   return (
    <FlatList 
     data={data}
     horizontal
     keyExtractor={(item) => item.id}
     renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pt-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>  
+        <TouchableOpacity 
+            style={tw`p-2 pt-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            onPress={() => navigation.navigate(item.screen)}
+        >  
             <View>
                 <Image 
                     style={{ width: 120, height: 120, resizeMode:"contain" }}
